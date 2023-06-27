4dfa585ca9c7e4b5fc007c7b36fc0fa48c1edb63
miqueiasmartinsf <126704294+miqueiasmartinsf@users.noreply.github.com>
2023-06-27 12:34:12 -0300

    new musics

--- scripts/music.js
+++ scripts/music.js
@@ -43,7 +43,7 @@ let music6 = {
     nome: 'Chapando O Côco 2',
     artista: 'Brandão085',
     url: '../audios/ChapandoOCôco2.mp3',
-    img: '../imagens/JovemDex.jpg',
+    img: '../imagens/brandao.jpg',
     favorite: false
 }
 
@@ -71,6 +71,59 @@ let music9 = {
     favorite:false
 }
 
-let listaMusicas = [music1,music2,music3,music4,music5,music6,music7,music8,music9];
+let music10 = {
+    nome: 'Rancor',
+    artista: 'Alee',
+    url: '../audios/Rancor.mp3',
+    img: '../imagens/alee.jpg',
+    favorite:false
+
+}
+
+let music11 = {
+    nome: 'Levantar Vôo',
+    artista: 'Alee',
+    url: '../audios/Levantar Vôo.mp3',
+    img: '../imagens/alee2.jpg',
+    favorite:false
+
+}
+
+let music12 = {
+    nome: 'Amor',
+    artista: 'Alee',
+    url: '../audios/Amor.mp3',
+    img: '../imagens/alee.jpg',
+    favorite:false
+
+}
+
+let music13 = {
+    nome: 'História',
+    artista: 'Alee',
+    url: '../audios/História.mp3',
+    img: '../imagens/alee2.jpg',
+    favorite:false
+}
+
+let music14 = {
+    nome: 'Afrobeat',
+    artista: 'Brandão 085',
+    url: '../audios/Afrobeat.mp3',
+    img: '../imagens/brandao.jpg',
+    favorite:false
+}
+
+let music15 = {
+    nome: 'Viagem',
+    artista: 'Alee',
+    url: '../audios/Viagem.mp3',
+    img: '../imagens/alee2.jpg',
+    favorite:false
+}
+
+
+
+let listaMusicas = [music1,music2,music3,music4,music5,music6,music7,music8,music9,music10,music11,music12,music13,music14,music15];
 
 export {listaMusicas};
